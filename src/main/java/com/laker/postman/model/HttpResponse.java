fbd5e7c624654134450bce0c58d9e680559d3b21
laker <935009066@qq.com>
2026-02-23 17:42:21 +0800

    bump: update version to 4.3.29 in pom.xml

--- src/main/java/com/laker/postman/model/HttpResponse.java
+++ src/main/java/com/laker/postman/model/HttpResponse.java
@@ -1,5 +1,7 @@
 package com.laker.postman.model;
 
+import com.laker.postman.service.http.EasyHttpHeaders;
+
 import java.util.List;
 import java.util.Map;
 
@@ -28,12 +30,12 @@ public class HttpResponse {
         if (headers == null) {
             return;
         }
-        if ("Easy-Content-Encoding".equalsIgnoreCase(name)) {
-            headers.put("Content-Encoding", value);
-            headers.remove("Easy-Content-Encoding");
-        } else if ("Easy-Content-Length".equalsIgnoreCase(name)) {
-            headers.put("Content-Length", value);
-            headers.remove("Easy-Content-Length");
+        if (EasyHttpHeaders.EASY_CONTENT_ENCODING.equalsIgnoreCase(name)) {
+            headers.put(EasyHttpHeaders.CONTENT_ENCODING, value);
+            headers.remove(EasyHttpHeaders.EASY_CONTENT_ENCODING);
+        } else if (EasyHttpHeaders.EASY_CONTENT_LENGTH.equalsIgnoreCase(name)) {
+            headers.put(EasyHttpHeaders.CONTENT_LENGTH, value);
+            headers.remove(EasyHttpHeaders.EASY_CONTENT_LENGTH);
         } else {
             headers.put(name, value);
         }
